28d5819b024580abc432e0686d6e2d6fc71575e8
DrLeyva <leyvaaaron2012@gmail.com>
2025-04-02 21:12:46 -0500

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st  # st is the module
+from pathlib import Path
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([1, 2])  # columns is the method  & col1 and col2 are the instances or objects
+
+with col1:
+    image_path = r"C:\Users\aaron\OneDrive\Desktop\resume\GitHub\PytonMegaCourse\app2-portfolio\images\photo2.png"
+    st.image(image_path, width=400)
+with col2:
+    st.title("Aaron Leyva")
+    content = """
+   My name is Aaron Leyva. I am python programmer looking to advance in the world of software development and machine learning engineering. 
+
+    """
+    # You can use st.markdown for custom styling
+    # st.markdown(f"<div style='font-size: 18px; line-height: 1.4;'>{content}</div>", unsafe_allow_html=True)
+
+    st.info(content)
